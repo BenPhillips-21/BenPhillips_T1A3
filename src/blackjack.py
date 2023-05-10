82b80f5a1c8756ace00bc1d0f12b1a4beff937b6
HolidayGuy21 <14449@coderacademy.edu.au>
2023-05-10 12:04:48 +1000

    created deck of cards, created dealer and user hands

--- src/blackjack.py
+++ src/blackjack.py
@@ -1,4 +1,9 @@
-x = 5
-y = 5
-z = x + y
-print(z)
+import random
+
+# deck of cards / player dealer hand
+
+deck = [2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,
+        'A', 'A', 'A', 'A', 'J', 'J', 'J', 'J', 'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K']
+dealer_hand = []
+user_hand = []
+
