1aebb483a6095f34d7de022065446ee1a43c92e5
HolidayGuy21 <14449@coderacademy.edu.au>
2023-05-10 13:23:20 +1000

    added 'hit' function

--- src/blackjack.py
+++ src/blackjack.py
@@ -1,6 +1,8 @@
 import random
 import sys
 
+
+
 def calculate_hand(user_hand):
     result = 0
     for item in user_hand:
@@ -15,7 +17,32 @@ def calculate_hand(user_hand):
             result += int(item)
     return result
 
+def play_again():
+    again = input("Do ye wish to play again? (y/n): ")
+    if again != "y":
+        print("Thanks for playing!")
+        sys.exit
+    else:
+        play_blackjack()
+
+def hit(user_input):
+    user_input = input("Shall ye hit or shall ye stand? (hit/stand) ")
+
+    if user_input == 'hit':
+        user_hand += random.sample(deck, 1)
+        result_of_user_hand = calculate_hand(user_hand)  
+        print(result_of_user_hand)
+        if result_of_user_hand == 21:
+            print("Blackjack! Ye win!")
+            play_again()
+        elif result_of_user_hand > 21:
+            print("Yer busted!")
+        else:
+            hit()
+
 # deck of cards / player dealer hand
+
+
 
 deck = [2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,
         'A', 'A', 'A', 'A', 'J', 'J', 'J', 'J', 'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K']
@@ -25,24 +52,25 @@ user_hand = []
 # deal card function
 
 dealer_hand += random.sample(deck, 2)
-
 user_hand += random.sample(deck, 2)
+
+
 print(f'Your hand: {user_hand}')
 result_of_user_hand = calculate_hand(user_hand)  
 print(f'({result_of_user_hand})')
 if result_of_user_hand == 21:
     print("Blackjack! Ye win!")
-    again = input("Do ye wish to play again? (y/n): ")
-    if again != "y":
-        print("Thanks for playing!")
-        sys.exit
-    else:
-        play_blackjack()
-
+    play_again()
 
 
 print(f"Dealer's hand: {dealer_hand[0]}")
-# input("Shall ye hit or shall ye stand? (hit/stand) ")
+
+user_input = input("Shall ye hit or shall ye stand? (hit/stand) ")
+
+if user_input == 'hit':
+    hit()
+elif user_input == 'stand':
+    stand()
 
 
 # calculate total of each hand
