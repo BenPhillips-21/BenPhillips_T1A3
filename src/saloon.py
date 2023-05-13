36d3c8835cb06116ac85923f8a941a60d25aa874
HolidayGuy21 <14449@coderacademy.edu.au>
2023-05-13 14:06:24 +1000

    implemented error handling

--- src/saloon.py
+++ src/saloon.py
@@ -13,17 +13,29 @@ def play_again():
             else:
                 print("Invalid input. Please enter 'y' or 'n'.")
 
-# from functions import hit, stand, play_again, calculate_hand
-
-print("You're sitting at a blackjack table in an old Saloon.")
-print("Across from you is the old dealer and a bad guy, twirling his moustache...")
-print("To your left is a very muscular man...")
-print("To your right is a young drunk big smile on his face...")
+print("The year is 1867, you're on the American Frontier sitting at a blackjack table in an old Saloon.")
+print("Across from you is the old dealer in a worn-out suit and a bad guy, who stares you down with his hand on his gun...")
+print("To your left is a silent, muscular man and to your right is a pathetic drunk babbling on...")
 while True:
-    choice = input("Do you want to play blackjack? (blackjack), talk to the muscular man? (muscles) the drunkard? (drunk) or the bad looking guy? (bad)\n")
+    while True:
+        try:
+            choice = input("Do you want to play blackjack? (blackjack), talk to the muscular man? (muscles), the drunkard? (drunk), or the bad guy? (bad)\n")
+            if choice.lower() not in ['blackjack', 'muscles', 'drunk', 'bad']:
+                raise ValueError("Input must be one of the strings (blackjack, muscles, drunk, bad)")
+            break
+        except ValueError as e:
+            print(e)
+
 
     if choice == "muscles":
-        response = input("You look like you go to the gym, you want me to help you find out how much protein you should be eating in order to reach your goals? (yes/no)")
+        while True:
+            try:
+                response = input("Do you want to know the ideal amount of protein you should eat that suits your fitness goals, pal? (yes/no)\n")
+                if response.lower() not in ['yes', 'no']:
+                    raise ValueError("Input must be either 'yes' or 'no'")
+                break
+            except ValueError as e:
+                print(e)
         if response == "yes":
 
             def protein_calculator(age, weight, gender, activity, goals):
@@ -60,17 +72,59 @@ while True:
                     play_again()
 
                 return protein
+            print("Alright. Well I'm going to have to ask you a few questions first...")
+            while True:
+                try:
+                    age = int(input("How old are you? "))
+                    break
+                except ValueError:
+                    print("Please enter a valid integer for your age.")
+                    
+            while True:
+                try:
+                    height = float(input("How tall are you in centimetres? "))
+                    break
+                except ValueError:
+                    print("Please enter a valid float for your height in centimetres.")
+                    
+            while True:
+                try:
+                    weight = float(input("How much do you weigh in kilograms? "))
+                    break
+                except ValueError:
+                    print("Please enter a valid float for your weight in kilograms.")
+                    
+            while True:
+                try:
+                    gender = input("My eye sight ain't too good, you a male or female? (male/female) ")
+                    if gender.lower() not in ['male', 'female']:
+                        raise ValueError("Please enter either 'male' or 'female' for your gender.")
+                    break
+                except ValueError as e:
+                    print(e)
+                    
+            while True:
+                try:
+                    activity = input("How active are you? (sedentary/lightly active/moderately active/very active/extremely active) ")
+                    if activity.lower() not in ['sedentary', 'lightly active', 'moderately active', 'very active', 'extremely active']:
+                        raise ValueError("Please enter a valid activity level.")
+                    break
+                except ValueError as e:
+                    print(e)
 
-            age = int(input("How old are you, in years that is?: "))
-            height = float(input("How tall are you in centimetres? "))
-            weight = float(input("How much do you weigh in kilograms? "))
-            gender = input("My eye sight ain't too good, you a male or female? ")
-            activity = input("How active are you? (sedentary/lightly active/moderately active/very active/extremely active): ")
-            goals = input("And are you trying to gain, maintain, or lose weight? (gain, maintain, lose)  ")
+            while True:
+                try:
+                    goals = input("And are you trying to gain, maintain, or lose weight? (gain, maintain, lose)  ")
+                    if goals.lower() not in ['gain', 'maintain', 'lose']:
+                        raise ValueError("Please enter either 'gain', 'maintain', or 'lose' for your weight goals.")
+                    break
+                except ValueError as e:
+                    print(e)
+
 
             protein_requirement = protein_calculator(age, weight, gender, activity, goals)
         else:
-            sys.exit()
+            play_again()
 
     elif choice == "blackjack": 
         def calculate_hand(user_hand):
@@ -87,18 +141,6 @@ while True:
                     result += int(item)
             return result
 
-
-        def play_again():
-            while True:
-                again = input("Do ye wish to play again? (y/n): ")
-                if again == "y":
-                    break
-                elif again == "n":
-                    sys.exit()
-                else:
-                    print("Invalid input. Please enter 'y' or 'n'.")
-
-
         def hit():
             global user_hand
             global result_of_user_hand
@@ -113,6 +155,7 @@ while True:
                 print("Yer busted!")
                 play_again()
             else:
+                
                 user_input = input("Shall ye hit again or shall ye stand? (hit/stand) ")
                 if user_input == 'hit':
                     hit()
@@ -178,7 +221,15 @@ while True:
 
             result_of_dealer_hand = calculate_hand(dealer_hand)
             print(f"Dealer's first card: {dealer_hand[0]}")
-            user_input = input("Shall ye hit or shall ye stand? (hit/stand) ")
+
+            while True:
+                    try:
+                        user_input = input("Shall ye hit or shall ye stand? (hit/stand) ")
+                        if user_input.lower() not in ['hit', 'stand']:
+                            raise ValueError("Input must be either 'hit' or 'stand'")
+                        break
+                    except ValueError as e:
+                        print(e)
 
             if user_input == 'hit':
                 hit()
@@ -196,7 +247,7 @@ while True:
         opening_line = [
         'Hey... hey buddy I got a joke for ya...',
         'Hey pal listen to this one...',
-        'Ay man I got a funny one...',
+        'Ay I got a funny one...',
         'BUUUURP sorry about that, wanna hear a joke?',
         'BUUUUURP'
         ]
@@ -209,13 +260,17 @@ while True:
 
     elif choice == "bad":
 
-        print("You've been lookin' at me funny this whole game. I'm thinking of a number between 1 and 20.")
+        print("You've been lookin' at me funny this whole game. I'm thinking of a number between 1 and 20...")
         print("You guess which number I'm thinkin' of within 3 tries, you live, otherwise...")
         print("You die.")
         random_number = random.randint(1, 15)
         for i in range(3):
-                
-            guess = int(input("What's your guess? "))
+            while True:
+                try:
+                    guess = int(input("What's your guess? "))
+                    break
+                except ValueError:
+                    print("Please enter a valid integer for your guess.")
             if guess == random_number:
                 print("You got it! You a lucky son of a gun, you know that?")
                 play_again()
